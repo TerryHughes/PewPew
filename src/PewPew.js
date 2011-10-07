29cedd4d59e9216093cf711afd227b3c6a0ce49b
TerryH <terry@tnthughes.net>
2011-10-06 21:47:59 -0700

    Implemented PewPew

---
+++ src/PewPew.js
@@ -0,0 +1,26 @@
+var files = [ "cheetah", "monkey", "bear", "ohmyyy" ];
+var sounds = [];
+
+function loadSounds() {
+    for (var i = 0; i < files.length; i++)
+        loadSound(files[i] + ".wav");
+}
+
+function loadSound(file) {
+    var sound = new Audio();
+    sound.src = "https://s3.amazonaws.com/PewPew/v1.0/" + file;
+    sound.load();
+
+    sounds.push(sound);
+}
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeObject) {
+    var random = Math.random() * 100;
+
+   if (random > 75)
+        return;
+
+    sounds[parseInt(random / 33)].play();
+});
+
+loadSounds();
